432828c7a8da32b03739de5d57e18f01ed7ce6ef
Nitin <tayalnitin008@gmail.com>
2025-02-07 11:49:02 +0530

    Initial login logut functionality added

---
+++ utils/database.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+export const connectToDB = async()=>{
+    mongoose.set('strictQuery', true);
+    var isConnected  = false;
+
+    if(isConnected){
+        console.log("Db is already conneted");
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt"
+        })
+
+        isConnected = true;
+        console.log("MongoB Connected")
+    } catch (error) {
+        console.log(error);
+    }
+}
